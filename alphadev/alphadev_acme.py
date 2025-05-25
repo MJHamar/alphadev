a7151ca02e093077c136f1794c1987b1483e8dee
Miklos Hamar <hamiki987@gmail.com>
2025-05-25 02:47:14 +0200

    remove legacy gpu memory growth flag

--- alphadev/alphadev_acme.py
+++ alphadev/alphadev_acme.py
@@ -23,17 +23,6 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 logger.setLevel(logging.INFO)
 
-# fix for memory problems
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            print("Setting memory growth for GPU: ", gpu)
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
-
 # #################
 # Agent definition
 # #################
